fe13a8212100b3979a56fd2736a4f845968991fd
vlasvlasvlas <vlasvlasvlas@gmail.com>
2022-10-11 21:52:17 -0300

    host port flask app

--- app.py
+++ app.py
@@ -37,5 +37,6 @@ app.layout = html.Div(
     ]
 )
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if __name__ == '__main__':
+    from os import environ
+    app.run(host='0.0.0.0', debug=False, port=environ.get("PORT", 5000))
